5627b5c6f0d4dca559ffdd0575533079f30c4f2d
Ruslan Ishmametov <russ-i@yandex.ru>
2018-02-24 19:46:31 +0300

    1.Функция фильтрации пользователей оп имени, фамилии, полу и городу 2.Функция блокировки отправки формы с пустыми полями jquery

--- filter_users.php
+++ filter_users.php
@@ -1,5 +1,11 @@
 <div class="filter_users_block col-md-3">
 	<form method="post" action="">
+		<!--<div class="form-group row">
+			<label for="example-text-input" class="col-2 col-form-label"><h5>ID</h5></label>
+			<div class="col-8">
+				<input class="form-control user_ids" type="text" name="user_ids" placeholder="Введите id" id="example-text-input" onkeyup='checkParams()'>
+			</div>
+		</div>-->
 		<div class="form-group row">
 			<label for="example-text-input" class="col-2 col-form-label"><h5>Имя</h5></label>
 			<div class="col-8">
@@ -13,7 +19,7 @@
 			</div>
 		</div>
 		<h5>Пол</h5>
-		<select name="sex" class="custom-select sex" onkeyup='checkParams()'>
+		<select name="sex" class="custom-select sex" onchange='checkParams()'>
 			<option selected>Выберите пол</option>
 			<option value="1">Женский</option>
 			<option value="2">Мужской</option>
